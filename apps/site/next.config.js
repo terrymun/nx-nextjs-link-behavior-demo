dada702c68a6e1baafb50c9935cc1ab6b0d7aa73
Terry Mun <terry.mun@lego.com>
2022-09-09 10:42:49 +0200

    feat: toggled newNextLinkBehavior

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -12,6 +12,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  experimental: {
+    newNextLinkBehavior: true,
+  }
 };
 
 module.exports = withNx(nextConfig);
--- apps/site/specs/index.spec.tsx
+++ apps/site/specs/index.spec.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render } from "@testing-library/react";
 
-import Index from '../pages/index';
+import Link from "next/link";
 
-describe('Index', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Index />);
-    expect(baseElement).toBeTruthy();
+describe("Index", () => {
+  it("should render <Link> element with new behavior", () => {
+    const { container } = render(
+      <Link href="https://example.com" data-testid="link">
+        Link element with new behavior
+      </Link>
+    );
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <a
+          href="https://example.com"
+        >
+          Link element with new behavior
+        </a>
+      </div>
+    `);
+  });
+
+  it("should render <Link> element with nested child with new behavior", () => {
+    const { container } = render(
+      <Link href="https://example.com" data-testid="link">
+        <strong data-testid="content">Link element with new behavior</strong>
+      </Link>
+    );
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <strong
+          data-testid="content"
+        >
+          Link element with new behavior
+        </strong>
+      </div>
+    `);
+  });
+
+  it("should render <Link> element with legacy behavior", () => {
+    const { container } = render(
+      <Link href="https://example.com" legacyBehavior>
+        <a href="#" data-testid="link">
+          Link element with legacy behavior
+        </a>
+      </Link>
+    );
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <a
+          data-testid="link"
+          href="#"
+        >
+          Link element with legacy behavior
+        </a>
+      </div>
+    `);
   });
 });
